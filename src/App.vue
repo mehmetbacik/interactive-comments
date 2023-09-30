3ab91f0b4a22c1ae85cf15b57f3365e85b708b1d
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-09-30 22:35:55 +0300

    Update
    Updated for content.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div v-for="comment in comments" :key="comment.id">
+      <p>{{ comment.content }}</p>
+      <button @click="toggleReply(comment)">Reply</button>
+      <div v-if="comment.showReply">
+        <input v-model="replyText" placeholder="Reply">
+        <button @click="submitReply(comment)">Send</button>
+      </div>
+      <button @click="editComment(comment.id)">Edit</button>
+      <button @click="deleteComment(comment.id)">Delete</button>
+    </div>
+    <div>
+      <input v-model="newComment" placeholder="Comment">
+      <button @click="addComment">Comment Add</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      comments: [],
+      newComment: '',
+      replyText: '',
+    };
+  },
+  methods: {
+    toggleReply(comment) {
+      comment.showReply = !comment.showReply;
+    },
+    submitReply(comment) {
+      comment.replies.push({ content: this.replyText });
+      this.replyText = '';
+    },
+    editComment(commentId) {
+      const comment = this.comments.find(comment => comment.id === commentId);
+      if (comment) {
+        const newText = prompt('Edit:', comment.content);
+        if (newText !== null) {
+          comment.content = newText;
+        }
+      }
+    },
+    deleteComment(commentId) {
+      const index = this.comments.findIndex(comment => comment.id === commentId);
+      if (index !== -1) {
+        this.comments.splice(index, 1);
+      }
+    },
+    addComment() {
+      if (this.newComment.trim() === '') return;
+      this.comments.push({
+        content: this.newComment,
+        createdAt: 'now',
+        id: new Date().getTime(),
+        replies: [],
+        score: 0,
+        user: {
+          image: {
+            png: '/images/avatars/image-juliusomo.png',
+            webp: '/images/avatars/image-juliusomo.webp',
+          },
+          username: 'juliusomo',
+        },
+        showReply: false,
+      });
+      this.newComment = '';
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
