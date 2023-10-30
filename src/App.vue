0028a42c64ffe247fefb1e78d41b195dc1a01df5
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-10-30 22:47:21 +0300

    Update
    Updated for component(pop-up).

--- src/App.vue
+++ src/App.vue
@@ -75,24 +75,19 @@
       <textarea v-model="newComment" class="comment-text" placeholder="Add a comment..." spellcheck="false"></textarea>
       <button @click="addComment">{{ buttonTexts.send }}</button>
     </div>
-    <div class="confirmation-popup overlay" v-if="confirmDelete">
-      <div class="confirmation-popup-content">
-        <span>Delete comment</span>
-        <p>Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
-        <div class="popup-button-group">
-          <button @click="cancelDeleteAction" class="cancel">{{ buttonTexts.cancel }}</button>
-          <button @click="confirmDeleteAction" class="confirm">{{ buttonTexts.confirm }}</button>
-        </div>
-      </div>
-    </div>
+    <ConfirmationPopup :confirmDelete="confirmDelete" :buttonTexts="buttonTexts" @cancel="cancelDeleteAction" @confirm="confirmDeleteAction" />
   </div>
 </template>
 
 <script>
 import jsonData from './assets/data/data.json';
 import DOMPurify from 'dompurify';
+import ConfirmationPopup from "./components/ConfirmationPopup.vue";
 
 export default {
+  components: {
+    ConfirmationPopup
+  },
   data() {
     return {
       comments: [],
---
+++ src/components/ConfirmationPopup.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="confirmation-popup overlay" v-if="confirmDelete">
+        <div class="confirmation-popup-content">
+            <span>Delete comment</span>
+            <p>Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
+            <div class="popup-button-group">
+            <button @click="cancelDeleteAction" class="cancel">{{ buttonTexts.cancel }}</button>
+            <button @click="confirmDeleteAction" class="confirm">{{ buttonTexts.confirm }}</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            confirmDelete: {
+                type: Boolean,
+                required: true
+            },
+            buttonTexts: {
+                type: Object,
+                required: true
+            }
+        },
+        methods: {
+            cancelDeleteAction() {
+                this.$emit("cancel");
+            },
+            confirmDeleteAction() {
+                this.$emit("confirm");
+            }
+        },
+    };
+</script>  
